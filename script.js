2a7609b8dc10baad22a24dbc283d8eb291557ffc
CJ Castillo <caytejohntims@gmail.com>
2022-05-11 18:20:59 +0800

    Create function to determine who will win for the round

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ function computerPlay() {
 
   return computerSelect;
 }
+
+// Function that will determine who will win the round between player's choice and computer's choice
+function playRound(playerSelection, computerPlay) {
+  if (playerSelection === computerPlay) {
+    return `You have same choice.`;
+  }
+  else if ((playerSelection === 'paper' && computerPlay === 'rock') ||
+    (playerSelection === 'scissors' && computerPlay === 'paper') ||
+    (playerSelection === 'rock' && computerPlay === 'scissors')) {
+    playerScore++;
+    return "You beat computer. PLAYER WIN!";
+
+  }
+  else if ((playerSelection === 'rock' && computerPlay === 'paper') ||
+    (playerSelection === 'scissors' && computerPlay === 'rock') ||
+    (playerSelection === 'paper' && computerPlay === 'scissors')) {
+    computerScore++;
+    return "Computer beats you. COMPUTER WIN!";
+  }
+}
