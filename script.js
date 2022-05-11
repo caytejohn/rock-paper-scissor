9f1b9164939b2242745321f55297cba0d8bcdb1a
CJ Castillo <caytejohntims@gmail.com>
2022-05-11 18:22:28 +0800

    Create function to ask user for the choice and track the winner of the game

--- script.js
+++ script.js
@@ -27,3 +27,26 @@ function playRound(playerSelection, computerPlay) {
     return "Computer beats you. COMPUTER WIN!";
   }
 }
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt('Choose one: "Rock", "Paper", "Scissors"').toLowerCase();
+    console.log(`You choose: ${playerSelection}`)
+    let computerSelect = computerPlay();
+    let rounds = playRound(playerSelection, computerSelect);
+    console.log(rounds);
+  }
+  console.log(`Computer Score: ${computerScore}`);
+  console.log(`Player Score: ${playerScore}`);
+
+  if (playerScore === computerScore) {
+    console.log(`It's Tie. Same Score`);
+  } else if (playerScore > computerScore) {
+    console.log(`You win the game. Your score is ${playerScore} while Computer scored ${computerScore}`);
+  } else {
+    console.log(`Computer wins the game. Computer scored ${computerScore} while you scored ${playerScore}`)
+  }
+  console.log("Game Over!");
+}
+
+game();
